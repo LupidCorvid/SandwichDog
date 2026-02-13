73f7e3b6db40c0d80e3f0bec53ffa6d9d8544428
LupidCorvid <lucidcanine921@gmail.com>
2026-02-13 15:30:27 -0500

    Coded spreading materials

--- Assets/Scripts/Object Classes/Silverware.cs
+++ Assets/Scripts/Object Classes/Silverware.cs
@@ -3,16 +3,39 @@ using UnityEngine;
 public class Silverware : ObjClass
 {
     public Spread currentSpread;
+    public GameObject mesh;
+    public Material[] matSpreads;
+
     public Silverware() : base(ObjType.GRABBABLE)
     {
         currentSpread = Spread.NOSPREAD;
     }
     public void addSpread(Spread s)
     {
+        //All spreads are shared as an array in the editor. The spread you want to add should correspond with the enum order.
         currentSpread = s;
+        if(mesh != null)
+        {
+            int index = (int)(s);
+            mesh.GetComponent<MeshRenderer>().material = matSpreads[index];
+        }
     }
     public void removeSpread()
     {
         currentSpread = Spread.NOSPREAD;
+        if (mesh != null)
+        {
+            mesh.GetComponent<MeshRenderer>().material = matSpreads[0];
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject col = collision.gameObject;
+        if (col.tag == "Grabbable")
+        {
+            if (col.GetComponent<Jar>() != null && currentSpread == Spread.NOSPREAD)
+                addSpread(col.GetComponent<Jar>().availSpread);
+        }
     }
 }
