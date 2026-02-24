bd59ce7d0acfd48f97423b3b3c9126c9344962e9
LupidCorvid <lucidcanine921@gmail.com>
2026-02-24 03:06:55 -0500

    Dog rotates with camera gaze

--- Assets/Scripts/FollowController.cs
+++ Assets/Scripts/FollowController.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class FollowController : MonoBehaviour
 {
+    public float forwardOffset = 0.2f;
+    public float rotationOffset = 180;
     public GameObject target;
+    public bool matchXRotation = false;
+    public bool matchYRotation = true;
+    public bool matchZRotation = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,9 +20,20 @@ public class FollowController : MonoBehaviour
     {
         if (target != null)
         {
+            //Match position
             Vector3 newPos = target.transform.position;
             newPos.y = gameObject.transform.position.y;
+            newPos.z -= forwardOffset;
             gameObject.transform.position = newPos;
+
+
+            //Match rotation
+            Vector3 newRot = target.transform.eulerAngles;
+            if (!matchXRotation) newRot.x = gameObject.transform.eulerAngles.x;
+            if (!matchYRotation) newRot.y = gameObject.transform.eulerAngles.y;
+            if (!matchZRotation) newRot.z = gameObject.transform.eulerAngles.z;
+            newRot.y -= 180;
+            gameObject.transform.eulerAngles = newRot;
             //gameObject.transform.rotation = target.transform.rotation;
         }
     }
