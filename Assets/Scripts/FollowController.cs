1c56f30122c623e447af0b2d54a1fba109553ff8
LupidCorvid <lucidcanine921@gmail.com>
2026-02-24 01:39:58 -0500

    Dog can move when you move the bones

---
+++ Assets/Scripts/FollowController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FollowController : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            gameObject.transform.position = target.transform.position;
+            gameObject.transform.rotation = target.transform.rotation;
+        }
+    }
+}
