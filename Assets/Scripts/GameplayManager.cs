d895feacf0833359a7eb3400869b05408f278ad0
LupidCorvid <lucidcanine921@gmail.com>
2026-02-24 18:09:34 -0500

    Prepared gameplayManager script

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -5,6 +5,8 @@ public class GameplayManager : MonoBehaviour
 {
     public const int maxLevels = 1; //How many levels the game has
     public int currentLevel = 1; //The current level the player is playing
+    public GameObject[] lvl1Reqs; //The objects required for scoring (prefabs dragged in via inspector)
+    public GameObject[] objectsToScore; //The objects the player brought to the end game area (should be added via this script)
 
     void Start()
     {
@@ -14,5 +16,13 @@ public class GameplayManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CalculateScore()
+    {
+        //First check which level the game is on
+        //If level 1...
+        //Check that the object is named "Bread"
+        //Check that one object has peanutbutter and one has jelly from Food (I.e. Food.Spread.JELLY)
     }
 }
