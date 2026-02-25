01ccfd95bb03d4648c60fb8139f5a94bd1843c5c
LupidCorvid <lucidcanine921@gmail.com>
2026-02-24 19:55:03 -0500

    Clamped velocity on bread and plate to manage jitter

---
+++ Assets/Scripts/ClampVelocity.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ClampVelocity : MonoBehaviour
+{
+    public float maxVel;
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVel);
+        if (rb.linearVelocity.magnitude > .005f) print(rb.linearVelocity.magnitude);
+    }
+}
