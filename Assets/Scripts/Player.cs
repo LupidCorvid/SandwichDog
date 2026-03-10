23472f530f6620980ab83f02833cca51fa846ee6
LupidCorvid <lucidcanine921@gmail.com>
2026-03-10 18:40:27 -0400

    Started stand toggle (camera snaps to new position)
    Debug key: C
    Applied playerInput component to xr origin, need to check if that will conflict with xr input at all

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,51 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
     public ObjClass highlighted;
+    public bool standing;
+
+    private float standScale = 1f;
+    private float crouchScale = .5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         highlighted = null;
+        standing = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (action)
+        //{
+        //    if (standing) Crouch();
+        //    else Stand();
+        //}
+    }
+
+    public void ToggleStand(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (standing)
+            {
+                standing = false;
+
+                //Set camera
+                gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, crouchScale, gameObject.transform.localScale.z);
+            }
+            else
+            {
+                standing = true;
+
+                //Set camera
+                gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, standScale, gameObject.transform.localScale.z);
+            }
+        }
     }
 }
