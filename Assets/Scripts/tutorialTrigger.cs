abfaab09d07f14cec8b9f239134c263fce70b14b
LupidCorvid <lucidcanine921@gmail.com>
2026-04-07 17:35:50 -0400

    Fixed tutorial advancing

--- Assets/Scripts/tutorialTrigger.cs
+++ Assets/Scripts/tutorialTrigger.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class tutorialTrigger : MonoBehaviour
 {
     [SerializeField] TutorialManager tutorialManager;
+    [SerializeField] int advanceCount; //How many steps to advance the tutorial to make it to the Ui that shows at this trigger specifically
+                                       //This is just in case the player doesn't interact with the book and walks into the kitchen immediately
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            //This is just in case the player doesn't interact with the book and walks into the kitchen immediately
-            //int advanceCount = 2 - tutorialManager.cursor;
-            //for (int i = 0; i < advanceCount; i++)
-            //{
-                tutorialManager.advanceTutorial();
-            //}
+            if (tutorialManager.cursor < advanceCount)
+            {
+                int count = advanceCount - tutorialManager.cursor; //lvl = 2
+                for (int i = 0; i < count; i++)
+                {
+                    tutorialManager.advanceTutorial();
+                }
+            }
             Destroy(gameObject);
         }
     }
