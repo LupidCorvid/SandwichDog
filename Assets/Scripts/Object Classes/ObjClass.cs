335f247a0a6184c3703583ebeb4bfc174ad2830b
LupidCorvid <lucidcanine921@gmail.com>
2026-04-10 15:02:44 -0400

    Fixed missing material error
    (Ily ternary operator and null coalescing operator)

--- Assets/Scripts/Object Classes/ObjClass.cs
+++ Assets/Scripts/Object Classes/ObjClass.cs
@@ -186,8 +186,8 @@ public class ObjClass : MonoBehaviour
 
     protected virtual void InitializeAppearanceLogic()
     {
-        objRenderer = GetComponent<MeshRenderer>();
-        cleanColor = objRenderer.material.GetColor("_BaseColor");
+        objRenderer ??= GetComponent<MeshRenderer>();
+        cleanColor = objRenderer != null ? objRenderer.material.GetColor("_BaseColor") : Color.red;
 
         if (currentSpread != Spread.NOSPREAD)
         {
