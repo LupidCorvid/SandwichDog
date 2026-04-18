d0b633a2db0b3a705b6bbf39ca81096c8fa4b3af
LupidCorvid <lucidcanine921@gmail.com>
2026-04-18 17:08:56 -0400

    Animated the tutorial arrow

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,31 +6,31 @@ using Unity.VisualScripting;
 
 public class TutorialManager : Singleton<TutorialManager>
 {
-    bool tutorialActive = false;
+    //bool tutorialActive = false;
     int tutorialLevel = 0; //Different instructions for different levels
 
     int maxSize = 0;
     public int cursor = -1; //Where you currently are in the tutorial
     GameObject lastArrow = null;
 
-    List<string> itemsThatTriggeredTutorial = new List<string>();
+    //List<string> itemsThatTriggeredTutorial = new List<string>();
 
-    public TutorialAssignment levelTutorialObjects;
+    public TutorialAssignment [] levelTutorialObjects;
 
-    public string[] lvl1Text = {
-        "Press and Hold both Side Triggers at the same time to pick up objects.",
-        "Press B to turn the page forward, and Y to turn the page backward.",
-        "Press X to swap walking style.",
-        "Press and Hold both Side Triggers at the same time to pick up objects.",
-        "Silverware can be held in one paw with the left or right Side Trigger.",
-        "Cabinets can be opened with one paw with the left or right Side Trigger.",
-        "When your sandwich is done, put it on a plate and place it on the table in the living room."};
+    //public string[] lvl1Text = {
+    //    "Press and Hold both Side Triggers at the same time to pick up objects.",
+    //    "Press B to turn the page forward, and Y to turn the page backward.",
+    //    "Press X to swap walking style.",
+    //    "Press and Hold both Side Triggers at the same time to pick up objects.",
+    //    "Silverware can be held in one paw with the left or right Side Trigger.",
+    //    "Cabinets can be opened with one paw with the left or right Side Trigger.",
+    //    "When your sandwich is done, put it on a plate and place it on the table in the living room."
+    //};
 
     void Start()
     {
-        tutorialActive = false;
-        maxSize = levelTutorialObjects.assignedTutorialObjs.Length; //7
-        startTutorial(1);
+        //tutorialActive = false;
+        //startTutorial(1);
     }
 
     float timer = 0f;
@@ -39,62 +39,66 @@ public class TutorialManager : Singleton<TutorialManager>
     {
 
         //Debug
-        //timer += Time.deltaTime;
-        //if (timer >= 3)
-        //{
-        //    timer = 0;
-        //    advanceTutorial();
-        //}
+        timer += Time.deltaTime;
+        if (timer >= 3)
+        {
+            timer = 0;
+            //advanceTutorial(1);
+        }
     }
 
     public void startTutorial(int level)
     {
-        tutorialActive = true;
+        //tutorialActive = true;
         tutorialLevel = level;
-        advanceTutorial();
+        maxSize = levelTutorialObjects[tutorialLevel - 1].assignedTutorialObjs.Length; //7
+        advanceTutorial(1);
     }
 
-    //TODO test on book
-    public void askToAdvanceTutorial(string ID)
+    //Once you interact with an object as instructed, it triggers the next tutorial event
+    //It moves the cursor to match the current object touched, which accomodates for people who skip the tutorial
+    public void askToAdvanceTutorial(int expectedCursor, int expectedGameLevel)
     {
-        if (!itemsThatTriggeredTutorial.Contains(ID))
+        //The object needs to be of the same level
+        //The object must proc a tutorial that hasn't appeared yet
+        if (expectedGameLevel == tutorialLevel && expectedCursor > cursor)
         {
-            advanceTutorial();
-            itemsThatTriggeredTutorial.Add(ID);
-            //if (ID == "book") advanceTutorial(); //debug
-            if (ID == "butterknife") advanceTutorial(); //debug
+            int numSteps = Math.Abs(cursor - expectedCursor);
+            advanceTutorial(numSteps);
         }
     }
 
-    public void advanceTutorial()
+    public void advanceTutorial(int steps)
     {
-        cursor += 1;
-        if (cursor < maxSize)
+        for (int i = 0; i < steps; i++)
         {
-            //Spawn new stuff
-            TutorialObj spawner = levelTutorialObjects.assignedTutorialObjs[cursor];
-            
-            //Delete the last arrow (keep UI text on screen for players to reference again)
-            if (lastArrow != null) Destroy(lastArrow);
-
-            //Some tutorial prompts dont require UI text. Position (0, 0, 0) signifies this.
-            if (spawner.UIPositionToSpawn != Vector3.zero)
+            cursor += 1;
+            if (cursor < maxSize)
             {
-                GameObject instructionUI = Instantiate(spawner.instructionUI, spawner.UIPositionToSpawn, Quaternion.identity);
-                instructionUI.GetComponentInChildren<Text>().text = spawner.UIText;
-                instructionUI.transform.Find("ControllerImage1").GetComponent<Image>().sprite = spawner.controllerImg1;
-                instructionUI.transform.Find("ControllerImage2").GetComponent<Image>().sprite = spawner.controllerImg2;
-            }
+                //Spawn new stuff
+                TutorialObj spawner = levelTutorialObjects[tutorialLevel - 1].assignedTutorialObjs[cursor];
 
-            //Some tutorial prompts dont require an arrow. Position (0, 0, 0) signifies this.
-            if (spawner.arrowPositionToSpawn != Vector3.zero)
-            {
-                lastArrow = Instantiate(spawner.arrow, spawner.arrowPositionToSpawn, Quaternion.identity);
-                lastArrow.transform.eulerAngles = new Vector3(90, 90, 0);
-            }
+                //Delete the last arrow (keep UI text on screen for players to reference again)
+                if (lastArrow != null) Destroy(lastArrow);
 
-            
+                //Some tutorial prompts dont require UI text. Position (0, 0, 0) signifies this.
+                if (spawner.UIPositionToSpawn != Vector3.zero)
+                {
+                    GameObject instructionUI = Instantiate(spawner.instructionUI, spawner.UIPositionToSpawn, Quaternion.identity);
+                    instructionUI.GetComponentInChildren<Text>().text = spawner.UIText;
+                    instructionUI.transform.Find("ControllerImage1").GetComponent<Image>().sprite = spawner.controllerImg1;
+                    instructionUI.transform.Find("ControllerImage2").GetComponent<Image>().sprite = spawner.controllerImg2;
+                }
+
+                //Some tutorial prompts dont require an arrow. Position (0, 0, 0) signifies this.
+                if (spawner.arrowPositionToSpawn != Vector3.zero)
+                {
+                    print("arrow");
+                    lastArrow = Instantiate(spawner.arrow, spawner.arrowPositionToSpawn, Quaternion.identity);
+                    lastArrow.transform.eulerAngles = new Vector3(90, 90, 0);
+                }
+            }
+            else break; //tutorialActive = false;
         }
-        else tutorialActive = false;
     }
 }
