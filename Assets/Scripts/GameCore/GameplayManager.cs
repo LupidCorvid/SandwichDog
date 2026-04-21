978ef7981f969118533c0b818400afd2cc640b79
LupidCorvid <lucidcanine921@gmail.com>
2026-04-21 11:23:11 -0400

    Made tutorialInvoker script

--- Assets/Scripts/GameCore/GameplayManager.cs
+++ Assets/Scripts/GameCore/GameplayManager.cs
@@ -11,7 +11,7 @@ using TMPro;
 public class GameplayManager : Singleton<GameplayManager>
 {
     public const int maxLevels = 2; //How many levels the game has
-    public static int currentLevel = 1; //The current level the player is playing //TODO: find a better way to do this
+    public int currentLevel = 1; //The current level the player is playing //TODO: find a better way to do this
 
     public Recipe_SO [] levelRecipe;
     public bool gameOver;
