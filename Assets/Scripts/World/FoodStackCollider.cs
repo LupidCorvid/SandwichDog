93c142261e8b434199dddf8afbbf655765c1624d
Luke Schultz <lukekatamari@gmail.com>
2026-05-08 11:48:27 -0400

    Move some sandwich creation code to SandwichBase instead of the collider

--- Assets/Scripts/World/FoodStackCollider.cs
+++ Assets/Scripts/World/FoodStackCollider.cs
@@ -15,11 +15,7 @@ public class FoodStackCollider : MonoBehaviour
         stackCollider.enabled = true;
     }
 
-    [SerializeField] GameObject emptySandwichObject;
-
-    public SandwichBase sandwichBase;
-    Sandwich sandwich;
-    private bool isInitializing = false; // due to coroutine during initialization
+    [SerializeField] private SandwichBase sandwichBase;
 
     // lerping info for prettier snapping over time
     [HideInInspector] public float timeToSnap;
@@ -33,61 +29,7 @@ public class FoodStackCollider : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(other.gameObject.name + " definitely entered collider!");
-        SandwichBase otherSandwichBase;
-        other.TryGetComponent<SandwichBase>(out otherSandwichBase);
-
-        if (otherSandwichBase)
-        {
-            // prevent unintentional creation of sandwiches
-            if (!otherSandwichBase.BaseFood.inHand && !this.sandwichBase.BaseFood.inHand) return;
-
-            // prevent forming purely bread sandwiches
-            if (otherSandwichBase.BaseFood.CurrentSpread == Spread.NO_SPREAD && this.sandwichBase.BaseFood.CurrentSpread == Spread.NO_SPREAD) return;
-
-            // whichever sandwich base is moving more will relinquish ownership to the other
-            if (this.sandwichBase.BaseFood.RigidBody.linearVelocity.magnitude < otherSandwichBase.BaseFood.RigidBody.linearVelocity.magnitude)
-            {
-                this.sandwichBase.DisableBothTriggers();
-                return;
-            }
-        }
-
-        Food targetFood = other.GetComponentInChildren<Food>();
-
-        if (!targetFood) return;
-        if (!targetFood.isStackable) return;
-        if (!targetFood.inHand && !this.sandwichBase.BaseFood.inHand) return;
-        if (sandwich)
-        {
-            if (targetFood.transform.IsChildOf(sandwich.transform)) return;
-        }
-
-        if (!sandwich && !isInitializing)
-        {
-            isInitializing = true;
-            sandwichBase.DisableOtherTrigger(this);
-            
-            GameObject sandwichOwner = Instantiate(emptySandwichObject);
-            sandwichOwner.transform.position = this.transform.position;
-            sandwich = sandwichOwner.GetComponent<Sandwich>();
-
-            sandwich.InitializeSandwich(this.sandwichBase, targetFood, this);
-            sandwich.EnableInteractability(); // starts as not interactable
-        }
-        else
-        {
-            Debug.Log("sandwich exists");
-            sandwich.PushNewFood(targetFood);
-
-        }
-        // TODO setup so that position starts to lerp in update
-    }
-
-    public void HandleDestroySandwich()
-    {
-
-        Destroy(sandwich);
+        sandwichBase.TryBuildSandwich(other.gameObject, this);
     }
 
     private void FixedUpdate()
